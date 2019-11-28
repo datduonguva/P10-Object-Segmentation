62df45cb09f010fccb35ee4885900d2cf58cc164
datduong <dqd8tp@virginia.edu>
2019-11-28 11:22:32 -0500

    continued analysis

--- src/pull.py
+++ src/pull.py
@@ -18,20 +18,39 @@ people_id = coco.getCatIds(catNms=['person'])
 
 image_ids = coco.getImgIds(catIds=people_id)
 
-images = coco.loadImgs(image_ids)
 
-a = {}
-for image in images:
-    annotation_ids = coco.getAnnIds(imgIds=image['id'], catIds=people_id, iscrowd=None)
-    len_ = len(annotation_ids)
-    if len_ not in a:
-        a[len_] = 1
-    else:
-        a[len_] += 1
-print(a)
+def plot_image(image_json):
+    annotation_ids = coco.getAnnIds(imgIds=image_json['id'], catIds=people_id, iscrowd=False)
+    annotations = coco.loadAnns(annotation_ids)
+
+    image = cv2.imread(os.path.join("{}/{}/{}".format(data_dir, data_type, image_json['file_name'])))
+
+    for annotation in annotations:
+        segnments = np.array(annotation['segmentation'][0]).astype(int)
+        for i in range(len(segnments)//2-1):
+            image = cv2.line(image, 
+                             (segnments[i*2], segnments[2*i + 1]),
+                             (segnments[i*2 + 2], segnments[2*i + 3]),
+                             (255, 0, 0))
+    cv2.imshow('image', image)
+    cv2.waitKey()
+
+def area_analysis(image_ids):
+    """ 
+    This is to understand the distribution of number of objects in each images
+    """
+
+    images = coco.loadImgs(image_ids)
+    for image in images:
+        annotation_ids = coco.getAnnIds(imgIds=image_json['id'], catIds=people_id, iscrowd=False)
+        annotations = coco.loadAnns(annotation_ids)
+
+
+
+if __name__ == "__main__":
+    #plot_image(images[np.random.randint(len(images))])
 
 raise SystemExit
-annotation = coco.loadAnns(annotation_id)
 for item in annotation:
     print("ratio: {}".format(item['area']/img['height']/img['width']))
 
