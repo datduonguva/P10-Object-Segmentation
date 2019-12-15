9dbd54977e0138cdfc9586632037194b48cf6e6b
datduong <dqd8tp@virginia.edu>
2019-12-15 09:54:14 -0500

    remove the flooded

--- src/cv2_experiment.py
+++ src/cv2_experiment.py
@@ -1,4 +1,5 @@
 import cv2
+import sys
 import numpy as np
 
 def average_contour(cnt):
@@ -8,7 +9,7 @@ def average_contour(cnt):
     for i in range(1, n):
         px = (cnt[(i-1)%n][0] + 2*cnt[i%n][0] + cnt[(i+1)%n][0])/4
         py = (cnt[(i-1)%n][1] + 2*cnt[i%n][1] + cnt[(i+1)%n][1])/4
-        result.append([px, py])
+        result.append([int(px), int(py)])
 
     return result
 
@@ -18,31 +19,34 @@ def find_contours(mask_255, threshold=100):
     return the contours of the white regions
     """
 
-    ret, image= cv2.threshold(mask_255, 100, 255,0)
-    image = image.copy()
+    ret, image= cv2.threshold(mask_255, threshold, 255,0)
+#    image = image.copy()
+#
+#    # fill the small holes in the images
+#    t1 = (image[:-2, :] == 255)
+#    t2 = (image[1:-1, :] == 0)
+#    t3 = (image[2:, :] == 255)
+#    t4 = np.logical_and(t1 == t2, t1 == t3) + 0
+#    t5 = np.pad(t4, ((1, 1), (0, 0)), 'constant', constant_values=0)
+#    image[t5 == 1] = 255
+#    
+#    t1 = (image[:, :-2] == 255)
+#    t2 = (image[:, 1:-1] == 0)
+#    t3 = (image[:, 2:] == 255)
+#    t4 = np.logical_and(t1 == t2, t1 == t3) + 0
+#    t5 = np.pad(t4, ((0, 0), (1, 1)), 'constant', constant_values=0)
+#    image[t5 == 1] = 255
+#
+#    flooded = cv2.floodFill(image.copy(), None, (0, 0), 255)
+#    flooded = 255 - flooded[1]
+#
+#   
+#    hole_filled_image = np.logical_or(image> threshold, flooded > threshold)
+#    hole_filled_image= (hole_filled_image*255.0).astype(np.uint8)
+#
 
-    # fill the small holes in the images
-    t1 = (image[:-2, :] == 255)
-    t2 = (image[1:-1, :] == 0)
-    t3 = (image[2:, :] == 255)
-    t4 = np.logical_and(t1 == t2, t1 == t3) + 0
-    t5 = np.pad(t4, ((1, 1), (0, 0)), 'constant', constant_values=0)
-    image[t5 == 1] = 255
     
-    t1 = (image[:, :-2] == 255)
-    t2 = (image[:, 1:-1] == 0)
-    t3 = (image[:, 2:] == 255)
-    t4 = np.logical_and(t1 == t2, t1 == t3) + 0
-    t5 = np.pad(t4, ((0, 0), (1, 1)), 'constant', constant_values=0)
-    image[t5 == 1] = 255
-
-    flooded = cv2.floodFill(image.copy(), None, (0, 0), 255)
-    flooded = 255 - flooded[1]
-
-    hole_filled_image = np.logical_or(image> threshold, flooded > threshold)
-    hole_filled_image= (hole_filled_image*255.0).astype(np.uint8)
-    
-    contours,hierarchy = cv2.findContours(hole_filled_image, 1, 2)
+    contours,hierarchy = cv2.findContours(image, 1, 2)
     contours = sorted(contours, key=lambda x: len(x), reverse=True)
     contours = [i for i in contours if len(i) > 20]
     
@@ -54,7 +58,6 @@ def find_contours(mask_255, threshold=100):
         result.append([[i[0], i[1]] for i in approx[:, 0]])
     
     # smooth out the contours:
-    print(result)
     result = [average_contour(cnt) for cnt in result]
     print("-------------------------")
     print(result)
@@ -76,8 +79,11 @@ def draw_contours(image, contours):
 
 
 if __name__ == '__main__':
-    image = cv2.imread('mask.jpg', cv2.IMREAD_GRAYSCALE)
+    image = cv2.imread('/home/datduong/Desktop/mask.png', cv2.IMREAD_GRAYSCALE)
 
-    contours = find_contours(image)
-
-    draw_contours(image, contours)
+    contours = find_contours(image, int(sys.argv[1]))
+    image2 = cv2.imread('/home/datduong/Desktop/mask.png')
+    image2 = draw_contours(image2, contours)
+    cv2.imshow('image2', image2)
+    cv2.waitKey() 
+    
